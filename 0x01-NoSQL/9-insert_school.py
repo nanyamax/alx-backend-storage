51d5966efcd7622ce8236e74f468972aca595ac2
nanyamax <ifunanyasunday7@gmail.com>
2023-10-16 15:13:00 -0700

    pymongo insert new document

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -3,6 +3,7 @@
 function that inserts a new doc in a collection
 """
 
+
 def insert_school(mongo_collection, **kwargs):
     new_kwargs = mongo_collection.insertOne(kwargs)
     return new_kwargs.inserted_id
